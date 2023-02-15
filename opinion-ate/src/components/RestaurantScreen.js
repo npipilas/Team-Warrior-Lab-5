c7b0c8e294ad5ccab546b6f1553811b9fef36dc0
yashpokra <yashpokra@gmail.com>
2023-02-15 18:15:03 -0500

    Add RestaurantScreen and RestaurantList

---
+++ opinion-ate/src/components/RestaurantScreen.js
@@ -0,0 +1,9 @@
+import RestaurantList from './RestaurantList';
+export default function RestaurantScreen() {
+    return (
+    <div>
+    <h1>Restaurants</h1>
+    <RestaurantList />
+    </div>
+    );
+    }
